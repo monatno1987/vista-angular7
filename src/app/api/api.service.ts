d025b142173a1545bcc90583422cb5b2fcb78953
Carlos Montano Guevara <carlos.montano@hab.desoft.cu>
2019-04-30 15:13:35 -0400

    asdasd

---
+++ src/app/api/api.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  apiURL = 'http://127.0.0.1:8000/api';
+  constructor(private http: HttpClient) { }
+ httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+    createSentence(words): Observable<any> {
+    return this.http.post<any>(this.apiURL + '/countwords', JSON.stringify(words), this.httpOptions)
+    .pipe(
+      retry(1),
+      catchError(this.handleError)
+    );
+  }
+
+  handleError(error) {
+     let errorMessage = '';
+     if(error.error instanceof ErrorEvent) {
+       // Get client-side error
+       errorMessage = error.error.message;
+     } else {
+       // Get server-side error
+       errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+     }
+     window.alert(errorMessage);
+     return throwError(errorMessage);
+  }
+
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, Input  } from '@angular/core';
+import { ApiService } from './api/api.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'wordscount';
+  // @Input() employeeDetails = { sentence: '', email: '', phone: 0 };
+    @Input() SentenceDetails = { sentence: ''};
+  constructor(
+    public restApi: ApiService
+  ) { }
+
+  ngOnInit() { }
+
+    addSentence(dataSentence) {
+    this.restApi.createSentence(this.SentenceDetails).subscribe((data: {}) => {
+      console.log(data);
+    });
+  }
+
 }
