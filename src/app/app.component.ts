74a321e5f5c1e1fa70bb930e3e750fcf0c8df1c9
Carlos Montano Guevara <carlos.montano@hab.desoft.cu>
2019-05-02 15:40:12 -0400

    actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input  } from '@angular/core';
 import { ApiService } from './api/api.service';
+import {createElementCssSelector} from "@angular/compiler";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,17 +14,25 @@ export class AppComponent  implements OnInit {
     sent:string;
   constructor(
     public restApi: ApiService
+
   ) {
     this.dicc = [];
   }
 
   ngOnInit() { }
 
+
     addSentence(dataSentence) {
     this.sent = this.SentenceDetails.sentence;
     this.restApi.createSentence(this.SentenceDetails).subscribe((data: {}) => {
-        this.SentenceDetails.sentence = '';
-      this.dicc.push({sent: this.sent, result: JSON.stringify(data)});
+      console.log(data);
+      if (data===null)
+          this.dicc.push({sent: '', result: '{}'});
+      else {
+            this.SentenceDetails.sentence = '';
+            this.dicc.push({sent: this.sent, result: JSON.stringify(data)});
+        }
+
     });
   }
 
